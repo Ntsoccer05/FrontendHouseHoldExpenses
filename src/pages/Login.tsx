e3ccb4c6ad9b0b22dcef0e3a65f680422f0d6d10
Ntsoccer05 <2460socceronly0512@gmail.com>
2025-07-06 23:24:02 +0900

    Update login page title for improved clarity
    Revised the <title> tag in Login.tsx to better emphasize the app's ease of use and simplicity for users.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,7 @@ function Login() {
   return (
     <>
       <Helmet>
-        <title>家計簿アプリに簡単ログイン｜Googleで無料ではじめる「カケポン」</title>
+        <title>らくらく・シンプルに使える家計簿アプリに簡単ログイン｜Googleで無料ではじめる「カケポン」</title>
         <meta name="description" content="【完全無料】簡単ログイン！カレンダー形式で支出・収入を簡単に記録・分析できる家計簿アプリ「カケポン」。主婦・社会人・カップル・学生・一人暮らしの節約に人気！" />
         <meta property="og:title" content="簡単ログイン｜完全無料カレンダー家計簿アプリ「カケポン」" />
         <meta
